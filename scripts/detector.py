71bee10523a4b301eca07c165028b1f5c93146fb
allysonburba <allyson.burba@gmail.com>
2024-06-28 16:21:50 +0100

    add launch file & prep for env var

--- scripts/detector.py
+++ scripts/detector.py
@@ -130,7 +130,7 @@ def ros_wrapper(objects):
     # When working on chair, uncomment 1st line and comment 2nd line
     # ros_msg.header.frame_id = CAMERA_NAME + "_left_camera_frame"
     ros_msg.header.frame_id = CAMERA_NAME
-    
+
     obj_list = []
     for obj in objects.object_list:
         obj_msg = zed_msgs.Object()
@@ -191,7 +191,8 @@ def local_to_map_transform(msg, tfBuffer):
                 # corner.kp = tf2_geometry_msgs.do_transform_point(p, transform)
                 # corner.kp = [corner.kp.point.x, corner.kp.point.y, corner.kp.point.z]
                 corner.kp = [p.point.x, p.point.y, p.point.z]
-
+        # uncomment 1st line when working on chair
+        # msg.header.frame_id = "map"
         msg.header.frame_id = CAMERA_NAME
     except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
         print("Failed to transform object from local to map frame")
